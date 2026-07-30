3210621c295977e04b929a279690b9e050caa01c
Sachinyadavsk <sachinsk4832499@gmail.com>
2026-07-30 18:13:45 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,16 @@
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.get("/", (req, res) => {
+    res.send("Book Library Store API");
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    connectDB();
+    console.log(`Server running on port ${PORT}`);
+});

5f73cb6a39d549c1858b02ee07fcc92b31b8aaf0
Sachinyadavsk <sachinsk4832499@gmail.com>
2026-08-05 16:41:59 +0530

    update the auth routes

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,13 +1,35 @@
 import express from "express";
 import auth from "../middleware/auth.js";
 import admin from "../middleware/admin.js";
-import { login, getProfile, register } from "../controllers/authController.js";
+import {
+    register,
+    login,
+    refreshToken,
+    forgotPassword,
+    resetPassword,
+    verifyEmail,
+    resendVerificationEmail,
+    logout,
+    changePassword,
+    getProfile,
+    updateProfile,
+} from '../controllers/authController.js'
 
 const router = express.Router();
 
 router.post("/register", register);
 router.post("/login", login);
+router.post("/refresh-token", refreshToken);
+router.post("/forgot-password", forgotPassword);
+router.post("/reset-password", resetPassword);
+router.get("/verify-email/:token", verifyEmail);
+router.post("/resend-verification-email", resendVerificationEmail);
+
+// Protected Routes
+router.post("/logout", auth, logout);
+router.post("/change-password", auth, changePassword);
 router.get("/profile", auth, getProfile);
+router.put("/profile", auth, updateProfile);
 
 // admin side routes
 router.get("/dashboard", auth, admin, (req, res) => {

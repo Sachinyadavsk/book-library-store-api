d0d9c6ff84b96a175e1009550471070160d26603
Sachinyadavsk <sachinsk4832499@gmail.com>
2026-08-20 16:53:14 +0530

    backend formatrd

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
+import cors from 'cors';
 
 // routes path 
 import userRouters from './routes/userRoutes.js';
@@ -20,6 +21,7 @@ import wishlistRoutes from './routes/wishlistRoutes.js'
 
 dotenv.config();
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.get("/", (req, res) => {
     res.send("Book Library Store API");
